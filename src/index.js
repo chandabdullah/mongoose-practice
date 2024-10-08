a33d24dab9672c5d3e74968197cddad44aba823c
Abdullah <chandabdullah.nls@gmail.com>
2024-10-08 08:59:12 +0000

    cors & cookie parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 // require('dotenv').config({path: './env'});
 import dotenv from 'dotenv';
 import connectDatabase from "./database/index.js";
+import { app } from './app.js';
 
 dotenv.config({ path: './env' })
 
-connectDatabase();
+connectDatabase()
+    .then(() => {
+        const PORT = process.env.PORT || 8000;
+        app.listen(process.env.PORT || 8000,()=>{
+            console.log(`Server is running at port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("Mongo database connection failed: ", err);
+
+    });
 
 
 // ; (async () => {
