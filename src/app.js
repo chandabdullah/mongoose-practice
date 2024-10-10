1047cda7dce09af6dd7a7f22b753649fb27ef20d
Abdullah <chandabdullah.nls@gmail.com>
2024-10-10 18:50:56 +0000

    user route and controller

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes
+import userRouter from "./routes/user.route.js";
+
+// routes declaration
+app.use("/api/v1/user", userRouter);
 
 export { app };
